4c7d6006d00845926c1a4f90481e2aeee6b4275d
Raelene Ho <raeyean@gmail.com>
2020-09-06 20:58:10 +1000

    updated photo upload function on show form

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   has_many :reviews
+  has_many_attached :photos
   validates :name, presence: true, uniqueness: true
 end
